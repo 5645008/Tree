05dfca80c175a78cf667fef3c569e8cc4037ce41
5645008 <kangbumsuck@gmail.com>
2023-09-21 10:47:36 +0900

    thread-0921

--- Tree/Tree.cpp
+++ Tree/Tree.cpp
@@ -1,117 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
-//링크 표현법
+
 typedef struct TreeNode {
 	int data;
 	struct TreeNode* left, * right;
-};
+	int is_thread;
+}TreeNode;
 
-TreeNode n1 = { 4,NULL, NULL };
-TreeNode n2 = { 5,NULL, NULL };
-TreeNode n3 = { 10,NULL, NULL };
-TreeNode n4 = { 11,NULL, NULL };
-TreeNode n5 = { 3,&n1, &n2 };
-TreeNode n6 = { 6,NULL, NULL };
-TreeNode n7 = { 8,NULL, NULL };
-TreeNode n8 = { 9,&n3, &n4 };
-TreeNode n9 = { 2,&n5, &n6 };
-TreeNode n10 = { 7,&n7, &n8 };
-TreeNode n11 = { 1,&n9, &n10 };
+//트리 구현
+TreeNode n1 = { 4,NULL, NULL,1};
+TreeNode n2 = { 5,NULL, NULL,1 };
+TreeNode n3 = { 10,NULL, NULL,1 };
+TreeNode n4 = { 11,NULL, NULL,0 };
+TreeNode n5 = { 3,&n1, &n2,0 };
+TreeNode n6 = { 6,NULL, NULL,1 };
+TreeNode n7 = { 8,NULL, NULL,1 };
+TreeNode n8 = { 9,&n3, &n4,0 };
+TreeNode n9 = { 2,&n5, &n6,0 };
+TreeNode n10 = { 7,&n7, &n8,0 };
+TreeNode n11 = { 1,&n9, &n10,0 };
 TreeNode* root = &n11;
 
-//링크-전위순회
-void link_preorder(TreeNode* root) {
-	if (root != NULL) {
-		printf("%d ", root->data);
-		link_preorder(root->left);
-		link_preorder(root->right);
-	}
+
+
+//중위 후속자를 찾는 함수
+TreeNode* find_successor(TreeNode* p) {
+	TreeNode* q = p->right;
+	if (q == NULL || p->is_thread == 1)
+		return q;
+	while (q->left != NULL)
+		q = q->left;
+	return q;
 }
-
-//링크-중위순회
-void link_inorder(TreeNode* root) {
-	if (root != NULL) {
-		link_inorder(root->left);
-		printf("%d ", root->data);
-		link_inorder(root->right);
-	}
-}
-
-//링크-후위순회
-void link_postorder(TreeNode* root) {
-	if (root != NULL) {
-		link_postorder(root->left);
-		link_postorder(root->right);
-		printf("%d ", root->data);
-	}
-}
-
-//배열 표현법
-int Tree[16] = { NULL, 1, 2, 7, 3, 6, 8, 9, 4, 5, NULL, NULL, NULL, NULL, 10, 11 };
-int array_root = Tree[1];
-
-//배열-전위순회
-void array_preorder(int root) {
-	if (Tree[root] != NULL) {
-		printf("%d ", Tree[root]);
-		array_preorder(root * 2);
-		array_preorder(root * 2 + 1);
-	}
-}
-
-//배열-중위순회
-void array_inorder(int root) {
-	if (Tree[root] != NULL) {
-		array_inorder(root * 2);
-		printf("%d ", Tree[root]);
-		array_inorder(root * 2 + 1);
-	}
-}
-
-//배열-후위순회
-void array_postorder(int root) {
-	if (Tree[root] != NULL) {
-		array_postorder(root * 2);
-		array_postorder(root * 2 + 1);
-		printf("%d ", Tree[root]);
-	}
+//스레드 버전 중위 순회 함수
+void thread_inorder(TreeNode* t)
+{
+	TreeNode* q;
+	q = t;
+	while (q->left)
+		q = q->left;
+	do
+	{
+		printf("%d ", q->data);
+		q = find_successor(q);
+	} while (q);
 }
 
 
-
-
+//부모노드를 찾는 함수
+TreeNode* parent(TreeNode* child) {
+	TreeNode* q = child;
+	if (q->is_thread == 1)
+		q = q->right;
+	if (q->left == child || q->right == child)
+		return q;
+}
 
 void main()
 {
-	//링크 표현법
-	printf("<Linked Tree>\n");
-	printf("1. 전위 순회\n");
-	link_preorder(root);
-	printf("\n\n");
+	n1.right = &n5;
+	n2.right = &n9;
+	n3.right = &n8;
+	n6.right = &n11;
+	n7.right = &n10;
 
-	printf("2. 중위 순회\n");
-	link_inorder(root);
-	printf("\n\n");
 
-	printf("3. 후위 순회\n");
-	link_postorder(root);
-	printf("\n\n");
 
-	//배열 표현법
-	printf("<Array Tree>\n");
-	printf("1. 전위 순회\n");
-	array_preorder(array_root);
+	printf("1. 중위 순회 결과\n");
+	thread_inorder(root);
 	printf("\n\n");
-
-	printf("2. 중위 순회\n");
-	array_inorder(array_root);
-	printf("\n\n");
-
-	printf("3. 후위 순회\n");
-	array_postorder(array_root);
-	printf("\n\n");
-
+	
+	TreeNode* a = parent(&n4);
+	TreeNode* b = parent(&n5);
+	TreeNode* c = parent(&n6);
+	printf("2. Node 4의 부모: %d \n", a->data);
+	printf("3. Node 5의 부모: %d \n", b->data);
+	printf("4. Node 6의 부모: %d ", c->data);
 
 
 }
